a957aeebc7d37b8c40aa419123048e7a815d0fdc
VladVes <trivsib@gamil.com>
2017-09-28 00:15:12 +0700

    update tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,16 +2,19 @@ import genDiff from '../src/';
 
 describe('compare files using imported genDiff', () => {
   const expected = `{\n   host: hexlet.io\n - timeout: 50\n - timeout: 20\n - proxy: 123.234.53.22\n - name: Sam\n - name: john\n + language: eng\n + verbose: true\n}`;
+  const fixtures = './__tests__/fixtures';
+  const before = `${fixtures}/before`;
+  const after = `${fixtures}/after`;
 
   it('should work whith JSON', () => {
-    expect(genDiff('./__tests__/fixtures/before.json', './__tests__/fixtures/after.json')).toBe(expected);
+    expect(genDiff(`${before}.json`, `${after}.json`)).toBe(expected);
   });
 
   it('should work whith YAML', () => {
-    expect(genDiff('./__tests__/fixtures/before.yml', './__tests__/fixtures/after.yml')).toBe(expected);
+    expect(genDiff(`${before}.yml`, `${after}.yml`)).toBe(expected);
   });
 
   it('should work whith INI', () => {
-    expect(genDiff('./__tests__/fixtures/before.ini', './__tests__/fixtures/after.ini')).toBe(expected);
+    expect(genDiff(`${before}.ini`, `${after}.ini`)).toBe(expected);
   });
 });
