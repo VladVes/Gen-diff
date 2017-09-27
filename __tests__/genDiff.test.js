ddb2745cc7c87a8f052e36e3d39c5a5b6a2ac148
VladVes <trivsib@gamil.com>
2017-09-27 20:00:18 +0700

    fix tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,7 +1,7 @@
 import genDiff from '../src/';
 
 describe('compare files using imported genDiff', () => {
-  const expected = `{\n   host: hexlet.io\n - timeout: 50\n - timeout: 20\n - proxy: 123.234.53.22\n + verbose: true\n}`;
+  const expected = `{\n   host: hexlet.io\n - timeout: 50\n - timeout: 20\n - proxy: 123.234.53.22\n - name: Sam\n - name: john\n + language: eng\n + verbose: true\n}`;
 
   it('should work whith JSON', () => {
     expect(genDiff('./__tests__/fixtures/before.json', './__tests__/fixtures/after.json')).toBe(expected);
